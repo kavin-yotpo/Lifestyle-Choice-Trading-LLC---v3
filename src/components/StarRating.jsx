3cf00448de676e98ce1906854bcb244f4c8d3163
Kavin-crew <abreganakavin@gmail.com>
2024-12-11 14:27:18 +0800

    removed collection stars

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -44,7 +44,6 @@ function StarRating(props) {
           data-bs-parent="#accordionExample"
         >
           <div className="accordion-body">
-
             <div className="rating-holder">
               <section>
                 <h1>Product Star Rating</h1>
@@ -53,16 +52,6 @@ function StarRating(props) {
                   data-yotpo-instance-id={instanceid}
                   data-yotpo-product-id={productid}
                   data-yotpo-section-id="product"
-                ></div>
-              </section>
-
-              <section>
-                <h1>Collections Star Rating</h1>
-                <div
-                  className="yotpo-widget-instance"
-                  data-yotpo-instance-id={instanceid}
-                  data-yotpo-product-id={productid}
-                  data-yotpo-section-id="collection"
                 ></div>
               </section>
             </div>
